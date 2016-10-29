a3b0d709f265078b5085f5b1fdaae9e77b8f4b63
MannyOh <emmaobo@yahoo.com>
2016-10-28 21:43:04 -0400

    Maven added

--- src/com/emmaobo/expensecalculator/Reader.java
+++
@@ -1,9 +0,0 @@
-package com.emmaobo.expensecalculator;
-
-/**
- * Created by emmao_000 on 10/20/2016.
- */
-public interface Reader {
-
-    public void readList();
-}
--- src/com/emmaobo/expensecalculator/Writer.java
+++
@@ -1,8 +0,0 @@
-package com.emmaobo.expensecalculator;
-
-import java.util.HashMap;
-
-public interface Writer {
-
-    void writeList(HashMap<String, Double> list);
-}
